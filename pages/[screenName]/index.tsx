e50f753a283aab38c0873255a5ec7d4e239663d1
이진국 <jinkook0126@naver.com>
2023-06-19 21:00:33 +0900

    feat: 리액트 쿼리 사용

--- pages/[screenName]/index.tsx
+++ pages/[screenName]/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-return-await */
 import {
   Avatar,
   Box,
@@ -16,6 +17,7 @@ import { GetServerSideProps, NextPage } from 'next';
 import ResizeTextArea from 'react-textarea-autosize';
 import { useEffect, useState } from 'react';
 import axios, { AxiosResponse } from 'axios';
+import { useQuery } from 'react-query';
 import { ServiceLayout } from '@/components/service_layout';
 import { useAuth } from '@/contexts/auth_user.context';
 import { InAuthUser } from '@/models/in_auth_user';
@@ -76,24 +78,24 @@ const UserHomePage: NextPage<Props> = function ({ userInfo }) {
   const [messageListFetchTrigger, setMessageListFetchTrigger] = useState(false);
   const toast = useToast();
   const { authUser } = useAuth();
-  async function fetchMessageList(uid: string) {
-    try {
-      const resp = await fetch(`/api/messages.list?uid=${uid}&page=${page}&size=5`);
-      if (resp.status === 200) {
-        const data: {
-          totalElements: number;
-          totalPages: number;
-          page: number;
-          size: number;
-          content: InMessage[];
-        } = await resp.json();
-        setTotalPage(data.totalPages);
-        setMessageList((prev) => [...prev, ...data.content]);
-      }
-    } catch (err) {
-      console.error(err);
-    }
-  }
+  // async function fetchMessageList(uid: string) {
+  //   try {
+  //     const resp = await fetch(`/api/messages.list?uid=${uid}&page=${page}&size=5`);
+  //     if (resp.status === 200) {
+  //       const data: {
+  //         totalElements: number;
+  //         totalPages: number;
+  //         page: number;
+  //         size: number;
+  //         content: InMessage[];
+  //       } = await resp.json();
+  //       setTotalPage(data.totalPages);
+  //       setMessageList((prev) => [...prev, ...data.content]);
+  //     }
+  //   } catch (err) {
+  //     console.error(err);
+  //   }
+  // }
   async function fetchMessageInfo({ uid, messageId }: { uid: string; messageId: string }) {
     try {
       const resp = await fetch(`/api/messages.info?uid=${uid}&messageId=${messageId}`);
@@ -113,10 +115,34 @@ const UserHomePage: NextPage<Props> = function ({ userInfo }) {
       console.error(err);
     }
   }
-  useEffect(() => {
-    if (userInfo === null) return;
-    fetchMessageList(userInfo.uid);
-  }, [userInfo, messageListFetchTrigger, page]);
+  const messageListQueryKey = ['messageList', userInfo?.uid, page, messageListFetchTrigger];
+  useQuery(
+    messageListQueryKey,
+    async () =>
+      await axios.get<{
+        totalElements: number;
+        totalPages: number;
+        page: number;
+        size: number;
+        content: InMessage[];
+      }>(`/api/messages.list?uid=${userInfo?.uid}&page=${page}&size=10`),
+    {
+      keepPreviousData: true,
+      refetchOnWindowFocus: false,
+      onSuccess: (data) => {
+        setTotalPage(data.data.totalPages);
+        if (page === 1) {
+          setMessageList([...data.data.content]);
+          return;
+        }
+        setMessageList((prev) => [...prev, ...data.data.content]);
+      },
+    },
+  );
+  // useEffect(() => {
+  //   if (userInfo === null) return;
+  //   fetchMessageList(userInfo.uid);
+  // }, [userInfo, messageListFetchTrigger, page]);
 
   if (userInfo === null) {
     return <p>사용자를 찾을 수 없습니다.</p>;
@@ -194,7 +220,10 @@ const UserHomePage: NextPage<Props> = function ({ userInfo }) {
                   toast({ title: '메시지 등록 실패', position: 'top-right' });
                 }
                 setMessage('');
-                setMessageListFetchTrigger((prev) => !prev);
+                setPage(1);
+                setTimeout(() => {
+                  setMessageListFetchTrigger((prev) => !prev);
+                }, 50);
               }}
             >
               등록
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,23 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import '../styles/globals.css';
 import type { AppProps /*, AppContext */ } from 'next/app';
+import { QueryClientProvider, QueryClient } from 'react-query';
+import { useRef } from 'react';
 import { AuthUserProvider } from '@/contexts/auth_user.context';
 
 const MyApp = function ({ Component, pageProps }: AppProps) {
+  const queryClientRef = useRef<QueryClient>();
+  if (!queryClientRef.current) {
+    queryClientRef.current = new QueryClient();
+  }
   return (
-    <ChakraProvider>
-      <AuthUserProvider>
-        <Component {...pageProps} />
-      </AuthUserProvider>
-    </ChakraProvider>
+    <QueryClientProvider client={queryClientRef.current}>
+      <ChakraProvider>
+        <AuthUserProvider>
+          <Component {...pageProps} />
+        </AuthUserProvider>
+      </ChakraProvider>
+    </QueryClientProvider>
   );
 };
 
