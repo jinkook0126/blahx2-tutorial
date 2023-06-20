516ccfa7d2380172e1cd7d5e3b790fa07f590410
이진국 <jinkook0126@naver.com>
2023-06-20 22:13:03 +0900

    feat: GNB 메뉴 수정

--- components/GNB.tsx
+++ components/GNB.tsx
@@ -1,14 +1,29 @@
-import { Box, Button, Flex, Spacer } from '@chakra-ui/react';
+import { Avatar, Box, Button, Flex, IconButton, Menu, MenuButton, MenuItem, MenuList, Spacer } from '@chakra-ui/react';
 
 import { useAuth } from '@/contexts/auth_user.context';
+import MoreBtnIcon from './more_btn_icon';
 
 const GNB = function () {
   const { loading, authUser, signOut, signInWithGoogle } = useAuth();
 
   const logOutBtn = (
-    <Button as="a" fontWeight={400} variant="link" onClick={signOut}>
-      로그아웃
-    </Button>
+    <Menu>
+      <MenuButton
+        as={IconButton}
+        icon={<Avatar size="md" src={authUser?.photoURL ?? 'https://bit.ly/broken-link'} />}
+        borderRadius="full"
+      />
+      <MenuList>
+        <MenuItem
+          onClick={() => {
+            window.location.href = `/${authUser?.email?.replace('@gmail.com', '')}`;
+          }}
+        >
+          사용자 홈으로 이동
+        </MenuItem>
+        <MenuItem onClick={signOut}>로그아웃</MenuItem>
+      </MenuList>
+    </Menu>
   );
   const logInBtn = (
     <Button
